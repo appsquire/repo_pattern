9c1d6122af8fecec2a93780a0e5d2a60124f4f40
Vic Rubba <crazedVic@users.noreply.github.com>
2024-02-02 13:35:17 -0700

    updated blog text to README.md

--- lib/shared/data.dart
+++ lib/shared/data.dart
@@ -1,7 +1,7 @@
 // grab your api key from https://api-ninjas.com/profile
 const apiKey= 'V4wOH25IuP8CrJf8m6AgdwCjBlZH3SzeiIgB9JHR';
 const baseUrl = 'api.api-ninjas.com';
-const DataSource dataAccessMode = DataSource.cache;
+const DataSource dataAccessMode = DataSource.api;
 
 enum DataSource {
   cache,
